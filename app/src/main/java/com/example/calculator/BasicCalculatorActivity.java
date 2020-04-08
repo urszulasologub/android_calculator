bbe536f4c8fdbe2f1647deab813700763dc21fe1
Urszula Sołogub <gold.ula4@gmail.com>
2020-04-08 13:26:47 +0200

    Added basic calculator layout

---
+++ app/src/main/java/com/example/calculator/BasicCalculatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class BasicCalculatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.basic_calculator);
+	}
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,43 +3,45 @@ package com.example.calculator;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        Button exit_button = (Button) findViewById(R.id.exit_button);
-        exit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                System.exit(0);
-            }
-        });
+		Button exit_button = (Button) findViewById(R.id.exit_button);
+		exit_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+				System.exit(0);
+			}
+		});
 
-        Button about_button = (Button) findViewById((R.id.about_button));
-        final AlertDialog.Builder about_dialog = new AlertDialog.Builder(this);
-        about_dialog.setTitle("About");
-        about_dialog.setMessage("Developed by Urszula Sołogub\ngithub.com/urszulasologub\n2020");
-        about_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                about_dialog.show();
-            }
-        });
+		Button about_button = (Button) findViewById((R.id.about_button));
+		final AlertDialog.Builder about_dialog = new AlertDialog.Builder(this);
+		about_dialog.setTitle("About");
+		about_dialog.setMessage("Developed by Urszula Sołogub\ngithub.com/urszulasologub\n2020");
+		about_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				about_dialog.show();
+			}
+		});
 
-        Button basic_button = (Button) findViewById(R.id.basic_button);
-        basic_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-    }
+		Button basic_button = (Button) findViewById(R.id.basic_button);
+		basic_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, BasicCalculatorActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
 }
